7ef35ce08c8a0a1cc0a81a2b20393269fcfabd0d
Faïssal <fhattou.ext@unowhy.com>
2023-12-18 15:41:46 +0100

    :fire:

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect } from "react";
+import axios from "axios";
+
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  useEffect(() => {
+    const getUser = async () => {
+      try {
+        const response = await axios.get("http://localhost:3002/auth/profile", {
+          withCredentials: true,
+        });
+
+        console.log("response", response.data);
+
+        return response.data;
+      } catch (error) {
+        console.log("error", error);
+      }
+    };
+
+    getUser();
+  }, []);
+
+  const loginWithGoogle = () => {
+    window.open("http://localhost:3002/auth/google/callback", "_self");
+  };
+
+  const logout = () => {
+    window.open("http://localhost:3002/auth/logout", "_self");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <button onClick={loginWithGoogle}>Sign in with Google</button>
+
+      <button onClick={logout}>Log out</button>
     </div>
   );
 }
